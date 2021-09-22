4f41f971b68608e9ee479e9cbc2bb9adf5760480
Woody3097 <nicolay.danch@gmail.com>
2021-09-22 15:25:05 +0300

    Added eslint and fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './modules/grid/grid.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, GridComponent],
+  imports: [BrowserModule, AgGridModule.withComponents([])],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
